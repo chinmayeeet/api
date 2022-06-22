c06e2e785087992bfa9d487378db082649864ef6
Chinmayee <55998927+chinmayeeet@users.noreply.github.com>
2022-06-22 17:04:21 +0530

    Update api.py

--- api.py
+++ api.py
@@ -2,8 +2,18 @@ import json
 from flask import Flask, send_file
 import code as code
 app = Flask(__name__)
-@app.route('/api/<filename>', methods=['GET'])
-def index(filename):
-    out = code.chpers(filename)
+
+
+@app.route('/api/<url>', methods=['GET'])
+def api_index(url):
+    # download the file from URL. Having unique name based on timestamp
+    # run the file on cv and save the output
+    out = code.chpers(url)
     return(out)
-app.run()
+
+@app.route('/api/<url>', methods=['GET'])
+def index(url):
+    out = code.chpers(url)
+    return(out)
+
+app.run(host='0.0.0.0', port=80, debug=True)
