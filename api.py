23f2796836e55bbc83e030764afc1dc9445bb688
Chinmayee <55998927+chinmayeeet@users.noreply.github.com>
2022-06-22 16:29:47 +0530

    Update api.py

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ import json
 from flask import Flask, send_file
 import code as code
 app = Flask(__name__)
-@app.route('/api/v1.0/<filename>', methods=['GET'])
+@app.route('/api/<filename>', methods=['GET'])
 def index(filename):
     out = code.chpers(filename)
     return(out)
