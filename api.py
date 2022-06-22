504c85076688c20a2ec2451dab106cd95f381ccc
Chinmayee <55998927+chinmayeeet@users.noreply.github.com>
2022-06-22 12:36:05 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,8 +1,9 @@
 import json
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_file
+import code as code
 app = Flask(__name__)
-@app.route('/api/v1.0/img/<int:task_id>', methods=['POST'])
-def index():
-    return jsonify({'name': 'alice',
-                       'email': 'alice@outlook.com'})
+@app.route('/api/v1.0/<filename>', methods=['GET'])
+def index(filename):
+    out = code.chpers(filename)
+    return send_file(out, mimetype='image/png')
 app.run()
