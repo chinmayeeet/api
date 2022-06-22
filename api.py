c95e87798819eb0b8c28820ce4d7eeed776b913c
Chinmayee <55998927+chinmayeeet@users.noreply.github.com>
2022-06-22 14:09:55 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,9 +1,9 @@
 import json
-from flask import Flask, jsonify, send_file
+from flask import Flask, send_file
 import code as code
 app = Flask(__name__)
 @app.route('/api/v1.0/<filename>', methods=['GET'])
 def index(filename):
     out = code.chpers(filename)
-    return send_file(out, mimetype='image/png')
+    return(out)
 app.run()
