6ade7256635e5cad2c72f69a8d227a198ac46aec
Chinmayee <55998927+chinmayeeet@users.noreply.github.com>
2022-06-23 16:37:27 +0530

    Update api.py

--- api.py
+++ api.py
@@ -53,9 +53,9 @@ def api_index(url, asImage=True):
                 response.raw.decode_content = True
                 shutil.copyfileobj(response.raw, f)  
 
-            img = Image.open(file_path)
-            out = fu.chpers(file_path, extension)
-            img.save(out, file_path)
+            # img = Image.open(file_path)
+            # out = fu.chpers(file_path, extension)
+            # img.save(out, file_path)
 
             url = base_url + file_name_for_regular_data + extension
 
