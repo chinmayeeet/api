d17e8433d73164b49370244d91e821235fbc58de
Chinmayee <55998927+chinmayeeet@users.noreply.github.com>
2022-06-23 16:23:54 +0530

    Update api.py

--- api.py
+++ api.py
@@ -28,51 +28,47 @@ def api_index(url, asImage=True):
     file_name_for_regular_data = url[-10:-4]
 
     dir_path = os.path.join(os.getcwdb().decode("utf-8"), "downloads") 
+    base_url = "http://35.200.208.107/downloads/"
+
+    result = {
+        "result": False,
+        "status": "unknown",
+        "url": "",
+    }
     
     # ----- SECTION 2 -----
     try:
-        # Base64 DATA
-        if "data:image/jpeg;base64," in url:
-            base_string = url.replace("data:image/jpeg;base64,", "")
-            decoded_img = base64.b64decode(base_string)
-            img = Image.open(BytesIO(decoded_img))
-
-            file_name = file_name_for_base64_data + ".jpg"
-            return(file_name)
-            #out = fu.chpers(file_name)
-            #img.save(out, "jpeg")
-
-        # Base64 DATA
-        elif "data:image/png;base64," in url:
-            base_string = url.replace("data:image/png;base64,", "")
-            decoded_img = base64.b64decode(base_string)
-            img = Image.open(BytesIO(decoded_img))
-
-            file_name = file_name_for_base64_data + ".png"
-            out = fu.chpers(file_name)
-            img.save(out, "png")
 
         # Regular URL Form DATA
-        else:
-            response = requests.get(url, stream=True)
-            if response.status_code == 200:
-                content_type = response.headers['content-type']
-                extension = mimetypes.guess_extension(content_type)
-                file_path = os.path.join(dir_path, file_name_for_regular_data + extension)
-                with open(file_path, 'wb') as f:
-                    response.raw.decode_content = True
-                    shutil.copyfileobj(response.raw, f)  
+        response = requests.get(url, stream=True)
+        if response.status_code == 200:
+            content_type = response.headers['content-type']
+            extension = mimetypes.guess_extension(content_type)
+            file_path = os.path.join(dir_path, file_name_for_regular_data + extension)
+            with open(file_path, 'wb') as f:
+                response.raw.decode_content = True
+                shutil.copyfileobj(response.raw, f)  
 
-                img = Image.open(file_path)
-                out = fu.chpers(file_path)
-                img.save(out, file_path)
+            img = Image.open(file_path)
+            out = fu.chpers(file_path)
+            img.save(out, file_path)
+
+            url = base_url + file_name_for_regular_data + extension
+
+            result["url"] = url
+            result["result"] = True
+            result["status"] = "success"
+        else:
+            result["url"] = ""
+            result["result"] = False
+            result["status"] = "URL not reachable"
         
     # ----- SECTION 3 -----    
         status = "Image has been succesfully sent to the server."
     except Exception as e:
         status = "Error! = " + str(e)
 
-    return status
+    return json.dumps(result, indent = 4) 
 
    
 
